3fce214134cefcf988cbd3412c06d6d2714d2fc2
Robert Pearce <me@robertwpearce.com>
2017-11-06 10:19:13 +1300

    explicltly list cask install apps

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -5,25 +5,21 @@
 brew tap caskroom/cask
 brew tap caskroom/versions
 
-# Apps
-apps=(
-  codekit
-  dropbox
-  firefox
-  flux
-  google-chrome
-  ifunbox
-  iterm2
-  keka
-  licecap
-  psequel
-  sketch
-  slack
-  spotify
-  vlc
-)
-
 # Install apps to /Applications
 # Default is: /Users/$user/Applications
 echo "Installing apps..."
-brew cask install "${apps[@]}"
+
+brew cask install codekit
+brew cask install dropbox
+brew cask install firefox
+brew cask install flux
+brew cask install google-chrome
+brew cask install ifunbox
+brew cask install iterm2
+brew cask install keka
+brew cask install licecap
+brew cask install psequel
+brew cask install sketch
+brew cask install slack
+brew cask install spotify
+brew cask install vlc
