f7fac4fbdb24b1381d78e9689ebe74fec7f7bd49
Robert Pearce <me@robertwpearce.com>
2019-05-08 12:32:39 +1200

    rm installs/vim-plug; update neovim/default.nix spacing

--- installs/vim-plug
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
--- programs/neovim/default.nix
+++ programs/neovim/default.nix
@@ -6,65 +6,65 @@ in {
   programs.neovim = {
     enable = true;
 
-		viAlias = true;
+    viAlias = true;
     vimAlias = true;
 
     configure = {
       customRC = builtins.readFile ./vimrc;
 
-			plug.plugins = with plugins; [
-				# Themes
-				onedark-vim
+      plug.plugins = with plugins; [
+        # Themes
+        onedark-vim
 
-				# Linting
-				ale
+        # Linting
+        ale
 
-				# Search
-				ack-vim
-				ctrlp-vim
+        # Search
+        ack-vim
+        ctrlp-vim
 
-				# Tools
-				nerdcommenter
-				nerdtree
-				supertab
-				tlib_vim
-				vim-addon-mw-utils
-				vim-buffergator
-				vim-css-color
-				vim-easymotion
-				vim-endwise
-				vim-eunuch
-				vim-fugitive
-				vim-gitgutter
-				vim-multiple-cursors
-				vim-snipmate
-				vim-trailing-whitespace
-				vim-vroom
+        # Tools
+        nerdcommenter
+        nerdtree
+        supertab
+        tlib_vim
+        vim-addon-mw-utils
+        vim-buffergator
+        vim-css-color
+        vim-easymotion
+        vim-endwise
+        vim-eunuch
+        vim-fugitive
+        vim-gitgutter
+        vim-multiple-cursors
+        vim-snipmate
+        vim-trailing-whitespace
+        vim-vroom
 
-				# Languages
-				vim-polyglot
-				#csv-vim
-        #elm-vim
-				#scss-syntax-vim
-				#typescript-vim
-				#vim-coffee-script
-				#vim-elixir
-				#vim-go
-				#vim-haml
-				#vim-haskell
-				#vim-javascript
-        #vim-jsx
+        # Languages
         vim-markdown
-				#vim-mustache-handlebars
+        vim-stylish-haskell
+        vim-polyglot
+        #csv-vim
+        #elm-vim
+        #scss-syntax-vim
+        #typescript-vim
+        #vim-coffee-script
+        #vim-elixir
+        #vim-go
+        #vim-haml
+        #vim-haskell
+        #vim-javascript
+        #vim-jsx
+        #vim-mustache-handlebars
         #vim-pug
-				#vim-rails
-				#vim-rspec
+        #vim-rails
+        #vim-rspec
         #vim-ruby
         #vim-ruby-minitest
-				#vim-scala
-				vim-stylish-haskell
-				#vimerl
-			];
+        #vim-scala
+        #vimerl
+      ];
     };
   };
 }
