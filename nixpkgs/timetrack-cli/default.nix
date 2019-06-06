f1397ffac17e17f894ab009dcd82e3ce929fe6cc
Robert Pearce <me@robertwpearce.com>
2019-06-06 11:38:08 -0600

    bump timetrack-cli: 0.1.0.4

--- nixpkgs/timetrack-cli/default.nix
+++ nixpkgs/timetrack-cli/default.nix
@@ -2,13 +2,13 @@
 
 haskellPackages.mkDerivation rec {
   pname = "timetrack-cli";
-  version = "0.1.0.3";
+  version = "0.1.0.4";
 
   src = fetchFromGitHub {
     owner = "rpearce";
     repo = "timetrack-cli";
     rev = "v${version}";
-    sha256 = "09z29i1pw6c6a2mwl9dnp121nq9ijw3891yffqw7ilb4rq83gbzh";
+    sha256 = "15186nljdmnh06rkabvrcgj6bs5qjwfcn5xd3c6d0ilbxjq4xal5";
   };
 
   isLibrary = false;
