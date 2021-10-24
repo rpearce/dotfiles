ea57f32ab7fd2103bf54c2ee90648b32e5436541
Robert W. Pearce <me@robertwpearce.com>
2021-10-24 19:31:00 -0400

    bump flake lock; use spanish firefox; comment out keynote because old version keeps requiring download

--- hosts/blueberry/homebrew.nix
+++ hosts/blueberry/homebrew.nix
@@ -30,7 +30,6 @@
     "docker"
     "dropbox"
     "element"
-    "firefox-developer-edition"
     "flux"
     #"font-hack-nerd-font"
     #"font-lilex"
@@ -56,7 +55,7 @@
     "1Password" = 1333542190;
     #Hush = 1544743900;
     "Just Focus" = 1142151959;
-    Keynote = 409183694;
+    #Keynote = 409183694;
     Kindle = 405399194;
     Medis = 1063631769;
     #Xcode = 497799835;
@@ -64,5 +63,7 @@
 
   homebrew.extraConfig = ''
     cask_args appdir: "~/Applications", require_sha: true
+
+    cask "firefox-developer-edition", args: { language: "es" }
   '';
 }
