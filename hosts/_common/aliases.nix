51d563688a5ee558c987d80bafa9732533a3c85d
Robert W. Pearce <me@robertwpearce.com>
2022-11-08 18:58:16 -0500

    Don't override cat with bat; drop ncdu for a minute because it's broken in nixpkgs macos

--- hosts/_common/aliases.nix
+++ hosts/_common/aliases.nix
@@ -1,7 +1,7 @@
 {
   environment.shellAliases = {
     # bat > cat
-    cat = "bat";
+    #cat = "bat";
 
     # Recursively remove Apple meta files
     cleanupds = "find . -type f -name '*.DS_Store' -ls -delete";
@@ -14,8 +14,8 @@
     dcr = "docker compose run --rm \${1}";
     dcrp = "dcr postgres psql postgresql://postgres@postgres:5432";
 
-    # ncdu > du
-    du = "ncdu --color dark -rr -x --exclude .git --exclude node_modules";
+    # Disk usage
+    dud = "ncdu --color dark -rr -x --exclude .git --exclude node_modules";
 
     # gpg
     gpgkill = "gpgconf --kill gpg-agent";
