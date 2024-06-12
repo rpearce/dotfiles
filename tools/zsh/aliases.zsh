1b74552fdec27388bf459882af0ec59e73bea82b
Robert W. Pearce <me@robertwpearce.com>
2024-06-13 10:13:17 +1200

    update gpg-fix

--- tools/zsh/aliases.zsh
+++ tools/zsh/aliases.zsh
@@ -64,7 +64,8 @@ fi
 
 # gpg
 if has_cmd gpg; then
-  alias gpg-fix="gpgconf --kill gpg-agent && gpg-agent --pinentry-program=$(which pinentry-mac) --daemon"
+  #alias gpg-fix="gpgconf --kill gpg-agent && gpg-agent --pinentry-program=$(which pinentry-mac) --daemon"
+  alias gpg-fix="gpgconf --kill gpg-agent && gpgconf --reload gpg-agent"
 fi
 
 # Network
