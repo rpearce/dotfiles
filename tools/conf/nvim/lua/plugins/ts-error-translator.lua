b006d7acdaa571b5db6fed7ecc4d51a0be9d75bd
Robert W. Pearce <me@robertwpearce.com>
2024-12-06 23:28:46 -0500

    ts-error-translator & lockfiles

---
+++ tools/conf/nvim/lua/plugins/ts-error-translator.lua
@@ -0,0 +1,16 @@
+return {
+  "dmmulroy/ts-error-translator.nvim",
+
+  event = "VeryLazy",
+
+  ft = {
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+  },
+
+  config = function()
+    require("ts-error-translator").setup()
+  end,
+}
