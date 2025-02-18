64201e4f0032d2d9dcacbdd3a0678d3b24275ccc
Robert W. Pearce <me@robertwpearce.com>
2025-02-17 22:10:28 -0500

    eslint autofix; silence hush

--- tools/conf/nvim/lua/plugins/lsp.lua
+++ tools/conf/nvim/lua/plugins/lsp.lua
@@ -128,6 +128,12 @@ return {
         eslint = function()
           lspconfig.eslint.setup({
             capabilities = capabilities,
+            on_attach = function(_, bufnr)
+              vim.api.nvim_create_autocmd("BufWritePre", {
+                buffer = bufnr,
+                command = "EslintFixAll",
+              })
+            end,
           })
         end,
 
