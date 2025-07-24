af053950c9c79865177a9232fc13d25a101e8b59
Robert W. Pearce <me@robertwpearce.com>
2025-07-25 10:38:05 +1200

    tldr -> tlrc; stop using ts-error-translator; claude global gitignore

--- tools/conf/nvim/lua/plugins/ts-error-translator.lua
+++ tools/conf/nvim/lua/plugins/ts-error-translator.lua
@@ -1,16 +1,16 @@
 return {
-  "dmmulroy/ts-error-translator.nvim",
-
-  event = "VeryLazy",
-
-  ft = {
-    "javascript",
-    "javascriptreact",
-    "typescript",
-    "typescriptreact",
-  },
-
-  config = function()
-    require("ts-error-translator").setup()
-  end,
+  -- "dmmulroy/ts-error-translator.nvim",
+  --
+  -- event = "VeryLazy",
+  --
+  -- ft = {
+  --   "javascript",
+  --   "javascriptreact",
+  --   "typescript",
+  --   "typescriptreact",
+  -- },
+  --
+  -- config = function()
+  --   require("ts-error-translator").setup()
+  -- end,
 }
