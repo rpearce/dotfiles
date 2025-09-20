8788c9fb77b057647cc49dd8cd4efd9b80a174fd
Robert W. Pearce <me@robertwpearce.com>
2025-09-20 12:18:52 -0400

    chore: take prettier into account as my main formatter

--- tools/conf/nvim/lua/plugins/formatting.lua
+++ tools/conf/nvim/lua/plugins/formatting.lua
@@ -18,9 +18,19 @@ return {
 
   opts = {
     formatters_by_ft = {
+      css = { "prettier" },
       go = { "gofumpt", lsp_format = "fallback" },
+      javascript = { "prettier" },
+      javascriptreact = { "prettier" },
+      json = { "prettier" },
       lua = { "stylua" },
+      markdown = { "prettier" },
       rust = { "rustfmt" },
+      sass = { "prettier" },
+      scss = { "prettier" },
+      typescript = { "prettier" },
+      typescriptreact = { "prettier" },
+      yaml = { "prettier" },
     },
     format_on_save = {
       timeout_ms = 500,
