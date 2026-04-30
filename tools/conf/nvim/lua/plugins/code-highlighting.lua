16a26b54c33619ec81834458f7783cd000963a97
Robert W. Pearce <me@robertwpearce.com>
2026-04-30 13:36:40 -0400

    fix tree-sitter stuff; add claude-code lua

--- tools/conf/nvim/lua/plugins/code-highlighting.lua
+++ tools/conf/nvim/lua/plugins/code-highlighting.lua
@@ -1,3 +1,59 @@
+local parsers = {
+  "astro",
+  "bash",
+  "c",
+  "css",
+  "dockerfile",
+  "elixir",
+  "embedded_template",
+  "haskell",
+  "heex",
+  "html",
+  "javascript",
+  "json",
+  "lua",
+  "markdown",
+  "markdown_inline",
+  "python",
+  "query",
+  "regex",
+  "ruby",
+  "rust",
+  "tsx",
+  "typescript",
+  "vim",
+  "vimdoc",
+  "yaml",
+}
+
+local filetypes = {
+  "astro",
+  "bash",
+  "c",
+  "css",
+  "dockerfile",
+  "elixir",
+  "eelixir",
+  "eruby",
+  "haskell",
+  "heex",
+  "html",
+  "javascript",
+  "json",
+  "lua",
+  "markdown",
+  "python",
+  "query",
+  "ruby",
+  "rust",
+  "sh",
+  "typescript",
+  "typescriptreact",
+  "vim",
+  "help",
+  "yaml",
+}
+
 return {
   "nvim-treesitter/nvim-treesitter",
   branch = "main",
@@ -5,37 +61,13 @@ return {
   build = ":TSUpdate",
 
   config = function()
-    require("nvim-treesitter").install({
-      "astro",
-      "bash",
-      "c",
-      "css",
-      "dockerfile",
-      "elixir",
-      "embedded_template",
-      "haskell",
-      "heex",
-      "html",
-      "javascript",
-      "json",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "python",
-      "query",
-      "regex",
-      "ruby",
-      "rust",
-      "tsx",
-      "typescript",
-      "vim",
-      "vimdoc",
-      "yaml",
-    })
+    require("nvim-treesitter").install(parsers)
 
     vim.api.nvim_create_autocmd("FileType", {
+      pattern = filetypes,
       callback = function()
-        pcall(vim.treesitter.start)
+        vim.treesitter.start()
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
       end,
     })
   end,
